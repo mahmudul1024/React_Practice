430dcf866b75f348eaab484ba51b06cb994ca4b9
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-06-08 22:39:09 +0200

    Jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const stringName="this is test one";
+const date= new Date(); //obect created of type Date 
+//which will be later used to access the Date methods
+//by calling its object date
+const currYear=date.getFullYear();
+const currMonth=date.getMonth();
+const currDay=date.getDay();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <>
+ <h1>Whtass app</h1>
+ <p>how is going : {stringName} </p>
+ <p>here will print current year : {currYear +"-"+currMonth +"-"+currDay}</p>
+ </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
