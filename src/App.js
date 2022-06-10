7906fb1390001b3349f89472bbb87bd29e880794
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-06-10 10:21:24 +0200

    nestedMapping

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import List from './components/Uniqe_list/Uniq_List';
 
-function App()
+const users=[
+    {
+        
+   fullName : "Mahmdudul Hasan",
+   age  : "29",
+   phones :[
+    {"home" : "017630169377"},
+    {office :"0201837"}
+   ],
+   
+        
+    fullName : "Mahmdudul Hasan",
+    age  : "29",
+    phones :[
+     {"home" : "017630169377"},
+     {office :"0201837"}
+    ]
+
+
+}]
+    
+   
+export default function App()
 {
     return <>
 
-     <List />
+{users.map((user,index)=>{
+    {/* use return when second bracket is used */}
+    return <div key={index}>
+       <p> {user.fullName}</p>
+       <p>{user.age}</p>
+       {user.phones.map((phone,index)=>
+       {
+        return <div key={index}>
+            <p>{phone.home}</p>
+            <p>{phone.office}</p>
+
+        </div>
+       })}
+    </div>
+})}
      
   
     </>
 }
 
-export default App;
