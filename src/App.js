5f8b91ffdfdb5fb6fc6d49de406b93fc141a2c50
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-06-10 08:56:24 +0200

    unique id

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import CardComponent from './components/Card';
-import DATA from './data.json';
+import List from './components/Uniqe_list/Uniq_List';
 
 function App()
 {
-   
-//     instead of for loop we will use map
-// for(let x=0;x<DATA.length;x++){
-//     items.push(<CardComponent textHeading={DATA[x].title} desc={DATA[x].desc} />)
-// }
-
-
-
-
     return <>
 
-   
-    <h1 className="headingStyle">How are you</h1>
-    {DATA.map((item, index)=> <CardComponent key={index} textHeading={item.title} desc={item.desc} />
-)}
-
-    {/* we will use for loop better instaed */}
-    {/* <CardComponent textHeading={DATA[0].title} desc={DATA[0].desc} />
-    <CardComponent textHeading={DATA[1].title} desc={DATA[1].desc} />
- */}
+     <List />
+     
+  
     </>
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CardComponent from './components/Card';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
