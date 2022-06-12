d3ee50c57d9e7d70a1b76d63bd4c3d456edc30dc
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-06-12 17:11:14 +0200

    Toggle

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,23 @@
-import React ,{useState} from 'react'
-import Todos from './Todos'
-import Style from './Home.module.css'
-import Newtodo from './NewTodo'
-import { v4 as uuidv4 } from 'uuid';
-
-// remains all the todo list 
-
+import React,{useState} from 'react'
 
 const Home = () => {
 
-  const [transferedTodos, setTransferedTodo]=useState([])
-
-// console.log("here will be transferedTodos")
-// console.log(transferedTodos)
-
-const handleTransfer=(e)=>{
-  console.log("Hi");
-  
-    setTransferedTodo((prevTodos )=>{
-      return [...prevTodos,{id:uuidv4(), e}]
-    })
-
- console.log(transferedTodos)
-
-}
-
-// const handleRemoveTodo=(id)=>{
-//   const filteredTodos=transferedTodos.filter((tran)=>
-//      tran.id !== id)
-//     setTransferedTodo(filteredTodos)
-  
-// }
-const handleRemoveTodo=(id)=>{
-  
-    setTransferedTodo((previousTodos)=>{
-      const filteredTodos=previousTodos.filter((tran)=>
-     tran.id !== id)
-     return filteredTodos;
-    })
-  
-}
-
-
+const [toggle,setToggle]=useState(true);
 
   return (
-    <div className={Style.container}>
-    {/* <h1 >{dummies[0].title}</h1> */}
-    <h1 style={{color : "wheat"}}>TODO APP</h1>
-    {/* parent er moddhe child print */}
-    <Newtodo onTransfer={handleTransfer} ></Newtodo>
-
-    {/* child NewTodo theke data props er maddhome parents er State Variable
-    transferedTodos a niye ashte hobe */}
-
-    <Todos todos_prop={transferedTodos} 
-    onRemoveTodo={handleRemoveTodo}></Todos>
-    </div>
+    <section style={{margin:"1rem", backgroundColor:"pink",
+    padding:"1rem" }}>
+      {toggle &&(<p >
+      Have you ever started to tell a joke only
+       to forget the punchline halfway through? 
+       While the forgetfulness could be funny on its own,
+        no one wants to suffer through the embarrassment of messing up a good joke
+      </p>)}
+      <section style={{textAlign: "center"}}>
+      <button onClick={()=>{setToggle(!toggle)}}>
+      {toggle ? "Hide" : "Show"}</button>
+      </section>
+    </section>
   )
 }
 
