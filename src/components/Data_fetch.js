f2b4f78eecbf6169828e36f30fb18b8abc881781
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-06-13 21:45:05 +0200

    Data fetch 2

--- src/components/Data_fetch.js
+++ src/components/Data_fetch.js
@@ -1,28 +1,51 @@
 import React, { useEffect, useState } from 'react'
 
+const loadingMessage=<p>todo is loading currently</p>
+
 const Data_fetch = () => {
 const [todos,setTodos]=useState(null)
+const [isLoading,setisLoading]=useState(true)
+const [error,setError]=useState(null)
 
 useEffect(()=>{
     setTimeout(() => {
         fetch('https://jsonplaceholder.typicode.com/todos')
-.then((res)=>{return res.json()
+.then((res)=>{
+    if(!res.ok){
+        throw Error("fetching is not successful")
+    }
+    else{
+        return res.json()
+    }
 
 }).then((data)=>{
     setTodos(data)
+    setisLoading(false)
+    setError(null)
     
+}).catch((error)=>{
+    setError(error.message)
+    setisLoading(false)
 })
-    }, 500);
+    }, 2000);
 
 },[])
+
+
+const todoElement=todos &&
+todos.map((todo)=>{
+    return <p key={todo.id}> {todo.title}</p>
+})
 
   return (
     <div>
       <h1>Todos</h1>
-      {todos &&
-        todos.map((todo)=>{
-            return <p key={todo.id}> {todo.title}</p>
-        })
+      {error && <p>{error}</p>}
+        {isLoading && loadingMessage}
+
+
+      {
+todoElement
       }
     </div>
   )
