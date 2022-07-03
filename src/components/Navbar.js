f045fc3a693b821c2df11ace1f9576cb5f6e9c9b
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-07-03 10:58:43 +0200

    untill here done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='nav'>
-    <h1 className='head1'>Port</h1>
+    <h1 className='head1'>Lebenslauf</h1>
       <NavLink to="/Home" className='nav-link'>Home</NavLink><br/>
       <NavLink to="/About"className='nav-link'>About</NavLink>
 
